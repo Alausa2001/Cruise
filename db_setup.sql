db5462d5664a121841a515244bcb5bcb6b9d9698
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2023-06-26 17:37:18 +0100

    missing semicolon fixed

--- db_setup.sql
+++ db_setup.sql
@@ -1,7 +1,7 @@
 -- create database and the user
 
--- SET GLOBAL validate_password.policy=LOW;
-GRANT CREATE, ALTER, DROP, REFERENCES ON *.* TO 'cruise'@'localhost'
+SET GLOBAL validate_password.policy=LOW;
+GRANT CREATE, ALTER, DROP, REFERENCES ON *.* TO 'cruise'@'localhost';
 CREATE DATABASE IF NOT EXISTS cruise_db;
 CREATE USER IF NOT EXISTS 'cruise'@'localhost' IDENTIFIED BY 'cruise_v1.0';
 GRANT ALL PRIVILEGES ON cruise_db.* TO 'cruise'@'localhost';
